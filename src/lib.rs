7ddd4cc036b43e6adf0082e758ec8a3583b7108f
Fionn Kelleher <fionn@sigkell.xyz>
2020-04-29 15:40:40 +0100

    add placeholder ircv3::client

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
 #![doc(html_logo_url="https://raw.githubusercontent.com/sigkell/ircv3-rs/master/logo.svg")]
 /// A set of parsing functions for the IRC protocol, and related formats.
 pub mod parsers;
+/// A full implementation of the core logic needed for an IRC client.
+pub mod client;
