5786a4204a4ee1c9ac741bd83f177917f97f5f2a
Fionn Kelleher <fionn@sigkell.xyz>
2020-04-29 15:40:03 +0100

    add description for ircv3::parsers

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,3 @@
 #![doc(html_logo_url="https://raw.githubusercontent.com/sigkell/ircv3-rs/master/logo.svg")]
+/// A set of parsing functions for the IRC protocol, and related formats.
 pub mod parsers;
